5805eeb535972ddab154bc95bbe819ee5e0d0e69
Ivan Premužić <ip49287@fer.hr>
2017-10-29 19:28:18 +0100

    Prvi zadatak

--- raupjc-hw2/Zad1/Class1.cs
+++ raupjc-hw2/Zad1/Class1.cs
@@ -2,7 +2,42 @@
 
 namespace Zad1
 {
-    public class Class1
+    public class Student
     {
+        public string Name { get; set; }
+        public string Jmbag { get; set; }
+        public Gender Gender { get; set; }
+
+        public Student(string name, string jmbag)
+        {
+            Name = name;
+            Jmbag = jmbag;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return this.Jmbag == (obj as Student)?.Jmbag;
+        }
+
+        public override int GetHashCode()
+        {
+            return Jmbag.GetHashCode();
+        }
+
+        public static bool operator ==(Student s1, Student s2)
+        {
+            return s1.Equals(s2);
+        }
+
+        public static bool operator !=(Student s1, Student s2)
+        {
+            return !s1.Equals(s2);
+        }
     }
+
+    public enum Gender
+    {
+        Male, Female
+    }
+
 }
