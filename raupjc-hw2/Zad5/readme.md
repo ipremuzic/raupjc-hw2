1cc5c718ddf37ff92beb9e3a777823208d1c8fff
Ivan Premužić <ip49287@fer.hr>
2017-11-02 11:35:45 +0100

    Zadaci 5, 6 i 7

---
+++ raupjc-hw2/Zad5/readme.md
@@ -0,0 +1,15 @@
+﻿#Pitanje 1:
+ Izvođenje programa je trajalo 5 sekundi.
+
+#Pitanje 2:
+ Operacije A, B, C, D i E su se izvodile na jednoj dretvi.
+
+#Pitanje 3:
+ Izvođenje programa je trajalo 1 sekundu.
+
+#Pitanje 4:
+ Operacije A, B, C, D i E su se izvodile svaka na zasebnoj dretvi, tj. operacije su se izvodile paralelno na pet dretvi.
+
+#Pitanje 5:
+ Recimo u slučaju da dvije dretve istovremeno dohvate vrijednost varijable. Nakon toga svaka dretva uveća vrijednost za jedan te npr. prva dretva spremi novu vrijednost i nakon toga druga spremi također tu istu vrijednost. Vrijednost varijable je uvećana za jedan, a ne za dva kao što bi se očekivalo, zbog toga što su dretve istovremeno pristupale zajedničkom sredstvu.
+ 
---
+++ raupjc-hw2/Zad6i7/Class1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Zad6i7
+{
+    public class Class1
+    {
+
+        public static async Task<int> FactorialDigitSum(int n)
+        {
+            return await Task.Run(() =>
+            {
+                int fact = 1;
+                while (n != 1)
+                {
+                    fact *= n;
+                    n -= 1;
+                }
+
+                char[] digits = fact.ToString().ToArray();
+
+                return digits.Sum(d => int.Parse(d.ToString()));
+            });
+
+        }
+
+        private static async Task LetsSayUserClickedAButtonOnGuiMethod()
+        {
+            var result = await GetTheMagicNumber();
+            Console.WriteLine(result);
+        }
+
+        private static async Task<int> GetTheMagicNumber()
+        {
+            return await IKnowIGuyWhoKnowsAGuy();
+        }
+
+        private static async Task<int> IKnowIGuyWhoKnowsAGuy()
+        {
+            return await IKnowWhoKnowsThis(10) + await IKnowWhoKnowsThis(5);
+        }
+
+        private static async Task<int> IKnowWhoKnowsThis(int n)
+        {
+            return await FactorialDigitSum(n);
+        }
+
+        // Ignore this part.
+        static void Main(string[] args)
+        {
+            // Main method is the only method that 
+            // can’t be marked with async.
+            // What we are doing here is just a way for us to simulate
+            // async-friendly environment you usually have with
+            // other .NET application types (like web apps, win apps etc.)
+            // Ignore main method, you can just focus on LetsSayUserClickedAButtonOnGuiMethod() as a
+            // first method in the call hierarchy. 
+            var t = Task.Run(() => LetsSayUserClickedAButtonOnGuiMethod());
+            Console.Read();
+        }
+    }
+}
